ef46f44737d7b2fe12f79603f01c5d5ba181c893
Cristopher Hernandez <DoughboysWWI@gmail.com>
2018-12-04 15:54:09 -0800

    Adding functions for target cell searching

--- project3.lisp
+++ project3.lisp
@@ -59,5 +59,36 @@
                     do (print (cadr (nth j (nth i maze_cp))))))
             maze_cp))   ; return new maze with values
 
+(defun get_neighbor_cells (i j cell_hist maze)
+    "Returns a list of lists where each element represents the coordinates
+    of a given option not found in cell_hist. The options are of the format:
+        (ROW COL)
+    parameters:
+        i - the row to search from
+        j - the column to search from
+        cell_hist - the history of cells to check
+        maze - the current maze data"
+    (let ((options '()))
+        (if (> i 0)
+            (if (not (member (car (nth j (nth (- i 1) maze))) cell_hist :test 'equal))
+                (setq options (cons (list (- i 1) j) options))))
+        (if (> j 0)
+            (if (not (member (car (nth (- j 1) (nth i maze))) cell_hist :test 'equal))
+                (setq options (cons (list i (- j 1)) options))))
+        (if (< (+ i 1) (length maze))
+            (if (not (member (car (nth j (nth (+ i 1) maze))) cell_hist :test 'equal))
+                (setq options (cons (list (+ i 1) j) options))))
+        (if (< (+ j 1) (length (nth i maze)))
+            (if (not (member (car (nth (+ j 1) (nth i maze))) cell_hist :test 'equal))
+                (setq options (cons (list i (+ j 1)) options))))
+        options))
+
+(defun choose_target_cell (agent maze)
+    (let ((opt_cells (get_neighbor_cells (nth 0 agent) (nth 1 agent) (nth 2 agent) maze)))
+        (if (not options)
+            ; TODO: Handle no options found that are not in history
+            ; TODO: Choose target cell based on heuristic 
+            )))
+
 (setq tester (read_maze "grid_a.txt"))
 (print (update_cells tester))
